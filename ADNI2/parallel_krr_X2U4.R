3eec41c510ec670662622aa2fb4a1c644bc7372f
peiyaow <monicawang76@gmail.com>
2019-02-19 12:32:28 -0500

    parallel

---
+++ ADNI2/parallel_krr_X2U4.R
@@ -0,0 +1,10 @@
+load("/nas/longleaf/home/peiyao/alpha/data/myseeds.RData")
+nsim = length(myseeds)
+
+for (i in 1:nsim){
+  for (gamma in 10^(-5:-2)){
+    system(paste0('sbatch -o main_', -log10(gamma), '.out --mem=2g -t 1:00:00 --wrap="Rscript krr_X2U4_longleaf.R gamma=', gamma, ' myseed=', myseeds[i], '"'))
+  }
+}
+
+
