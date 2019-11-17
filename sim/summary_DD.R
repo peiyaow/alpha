122dc2b53230bd0a4ed32676ac250e91b2d6be4b
peiyaow <monicawang76@gmail.com>
2019-11-16 22:36:20 -0500

    new simulation

--- sim/summary_DD.R
+++ sim/summary_DD.R
@@ -1,27 +1,38 @@
+# ridge
+load("/Users/MonicaW/Documents/GitHub/alpha/sim/results/ridge/DD.RData")
+# lasso
+load("/Users/MonicaW/Documents/GitHub/alpha/sim/results/lasso/DD.RData")
+
 n = 50
-DD_mean = do.call(rbind, lapply(0:12, function(k) apply(DD[1:n+k*n, ], 2, mean)))
-DD_sd = do.call(rbind, lapply(0:12, function(k) apply(DD[1:n+k*n, ], 2, sd)))/sqrt(n)
-x = seq(0, 6, by = 0.5)
+x = seq(0, 1, by = 0.1)
+KK = length(x)-1
+
+DD_mean = do.call(rbind, lapply(0:KK, function(k) apply(DD[1:n+k*n, ], 2, mean)))
+DD_sd = do.call(rbind, lapply(0:KK, function(k) apply(DD[1:n+k*n, ], 2, sd)))/sqrt(n)
 
 AA_mean = DD_mean
 AA_sd = DD_sd
 
-plot(x, AA_mean[,2], type = "l", 
-     ylim = c(28, 70), 
-     lwd = 1, lty = 4, col = "blue", xlab = "h", ylab = "error")
-polygon(c(x, rev(x)), c(AA_mean[,2]+AA_sd[,2], rev(AA_mean[,2]-AA_sd[,2])), col = rgb(0, 0, 255, 20, maxColorValue=255), border = FALSE)
+# global
+i = 1
+plot(x, AA_mean[,i], type = "l", 
+#     ylim = c(28, 70), 
+     lwd = 1, lty = 5, col = "green", xlab = "h", ylab = "error")
+polygon(c(x, rev(x)), c(AA_mean[,i]+AA_sd[,i], rev(AA_mean[,i]-AA_sd[,i])), col = rgb(0, 255, 0, 20, maxColorValue=255), border = FALSE)
 
-i = 3
+# groupwise
+i = 2
 #plot(AA_mean[,i], type = "l", ylim = c(9, 20))
 polygon(c(x, rev(x)), c(AA_mean[,i]+AA_sd[,i], rev(AA_mean[,i]-AA_sd[,i])), col = rgb(255, 0, 0, 20, maxColorValue=255), border = FALSE)
-lines(x, col = "red", AA_mean[,i], lwd = 1, lty = 3)
+lines(x, col = "red", AA_mean[,i], lwd = 1, lty = 4)
 
-i = 1
+# proposed
+i = 3
 #plot(AA_mean[,i], type = "l", ylim = c(9, 20))
-polygon(c(x, rev(x)), c(AA_mean[,i]+AA_sd[,i], rev(AA_mean[,i]-AA_sd[,i])), col = rgb(0, 255, 0, 20, maxColorValue=255), border = FALSE)
-lines(x, col = "green", AA_mean[,i], lwd = 1, lty = 5)
+polygon(c(x, rev(x)), c(AA_mean[,i]+AA_sd[,i], rev(AA_mean[,i]-AA_sd[,i])), col = rgb(0, 0, 255, 20, maxColorValue=255), border = FALSE)
+lines(x, col = "blue", AA_mean[,i], lwd = 1, lty = 3)
 
 legend("topleft", inset=.1, legend=c("Global", "Groupwise", "Proposed"),
-       col=c("green", "blue", "red"), lty=c(5, 4, 3), cex=1)
-
+       col=c("green", "red", "blue"), lty=c(5, 4, 3), cex=1)
+ 
 
