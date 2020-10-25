0818323e61de3871f360c2a564ada4cf3db633c2
Peiyao Wang <peiyaow76@gmail.com>
2020-10-25 14:02:15 -0700

    Update parallel.R

--- sim/parallel.R
+++ sim/parallel.R
@@ -1,3 +1,5 @@
-for (ii in 1:30){
-  system(paste0('sbatch -o main_', ii, '.out --mem=2g -t 2:00:00 --wrap="Rscript sim_main.R ii=', ii, '"'))
+load("~/alpha/data/myseeds0.RData")
+
+for (i in 1:50){
+  print(paste0('sbatch -o main.out --mem=4g -t 1:00:00 --wrap="Rscript sim.R myseed=', myseeds[i], '"'))
 }
